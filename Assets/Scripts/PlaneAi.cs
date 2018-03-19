b10001e54c80022a199a2157f165b780216fd0ec
Wellington Schütz <wellingtonschutz@outlook.com>
2018-03-18 21:42:28 -0300

    Add project files.

---
+++ Assets/Scripts/PlaneAi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneAi : MonoBehaviour 
+{
+
+	public float Velocidade = 3;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Translate(
+			Velocidade * Input.GetAxis("Horizontal") * Time.deltaTime, 
+			Velocidade * Input.GetAxis("Vertical") * Time.deltaTime, 
+			0);
+	}
+}
